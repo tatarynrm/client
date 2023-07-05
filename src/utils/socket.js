034b158948fb8747b21e7071318edd738bde49af
tatarynrm <tatarynrm@gmail.com>
2023-07-05 09:25:39 +0300

    ipv4

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 
-const socket = io("http://192.168.5.180:8800");
+// const socket = io("http://192.168.5.180:8800");
 // const socket = io("http://localhost:8800");
+const socket = io("http://0.0.0.0:8800");
 
 export default socket;
