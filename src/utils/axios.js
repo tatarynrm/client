c5381b90c2ae0852eeb1394736aeee78a17abe49
tatarynrm <tatarynrm@gmail.com>
2023-07-06 16:33:45 +0300

    local

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 const instance = axios.create({
   // baseURL: "http://localhost:8800",
-  // baseURL: "http://192.168.5.180:8800",
+  baseURL: "http://192.168.5.180:8800",
   // baseURL: "http://ict.lviv.ua/api",
-  baseURL: "http://api.ict.lviv.ua",
+  // baseURL: "http://api.ict.lviv.ua",
   // baseURL: "http://0.0.0.0:8800",
   // ..
-  headers: { 
-    'Access-Control-Allow-Origin' : '*',
-    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  },
+  // headers: { 
+  //   'Access-Control-Allow-Origin' : '*',
+  //   'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  // },
 });
 // this is comment
 instance.interceptors.request.use((config) => {
