e8c7fe1fad0526b65b5fee69595c93b4bab78351
tatarynrm <tatarynrm@gmail.com>
2023-07-06 14:15:59 +0300

    sockets

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,8 +1,8 @@
 import io from "socket.io-client";
 
-const socket = io("http://192.168.5.180:8800");
+// const socket = io("http://192.168.5.180:8800");
 // const socket = io("http://localhost:8800");
 // const socket = io("http://0.0.0.0:8800");
-// const socket = io("http://ict.lviv.ua/api");
+// const socket = io("http://api.ict.lviv.ua");
 
 export default socket;
