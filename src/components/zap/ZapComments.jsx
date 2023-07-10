7ec6df147ee5ac93a2731cf7768f1198bdf45c63
tatarynrm <tatarynrm@gmail.com>
2023-07-10 16:53:39 +0300

    tg bot

--- src/components/zap/ZapComments.jsx
+++ src/components/zap/ZapComments.jsx
@@ -47,6 +47,7 @@ const ZapComments = ({ showComments, selectedZap }) => {
             pComment: comment,
             zapAuthor: selectedZap.KOD_OS,
             pKodComment: data.data.outBinds.pKodCom,
+            telegramId:selectedZap.TELEGRAMID
           });
           socket.emit("myZapComment", {
             PIP: userData?.PIP,
