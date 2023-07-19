2b51e23aa736026313b81ee7021e860a8689f1b4
tatarynrm <tatarynrm@gmail.com>
2023-07-19 16:22:38 +0300

    auth tg otp

--- src/components/forms/login/LoginForm.jsx
+++ src/components/forms/login/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./LoginForm.scss";
 import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,13 +9,18 @@ import {
 } from "../../../redux/slices/auth";
 import { useNavigate } from "react-router-dom";
 import DarkMode from "../../darkMode/DarkMode";
+import axios from '../../../utils/axios'
 const LoginForm = () => {
   const [showPass, setShowPass] = useState(false);
   const [formData, setFormData] = useState({});
+  const [tgPass,setTgPass] = useState('')
+  const [tokenData,setTokenData] = useState(null)
   const dispatch = useDispatch();
   const isAuth = useSelector(selectIsAuth);
   const token = window.localStorage.getItem("token");
   const navigate = useNavigate();
+  const [userKod,setUserKod] = useState(null)
+  
   function setTokenDelete() {
     return setTimeout(() => {
       window.localStorage.removeItem("token");
@@ -35,17 +40,58 @@ const LoginForm = () => {
       return alert("Не вдалось авторизуватись");
     }
     if (data.payload.token) {
-      dispatch(fetchAuthMe());
-      window.localStorage.setItem("token", data.payload.token);
-      setTokenDelete();
+      setUserKod(data.payload.rows[0].KOD)
+      // console.log(data.payload.token);
+      setTokenData(data.payload.token)
     }
   };
+const tgCheck = async (e)=>{
+  e.preventDefault()
+  try {
+    const {data} = await axios.post('/auth/otp-code',{KOD_OS:userKod})
+    const OTP_VERIFY = data.rows[0].OTPCODE;
+    if (OTP_VERIFY == tgPass) {
+      dispatch(fetchAuthMe());
+      window.localStorage.setItem('token',tokenData);
+    }
+  } catch (error) {
+    
+  }
+}
+
+useEffect(()=>{
+  
+},[token])
+
 
   if (token) {
     return navigate("/");
   }
+
+
+
   return (
-    <form onSubmit={(e) => signIn(e)} className="login__form">
+<> {
+
+
+tokenData !== null ? 
+<form onSubmit={(e) => tgCheck(e)} className="login__form">
+<div className="form__control">
+  <label><b>TELEGRAM VERIFY</b></label>
+  <input
+    type={"text"}
+    placeholder="Пароль з телеграму"
+    name="tgpassword"
+    defaultValue={tgPass}
+    value={tgPass}
+    onChange={(e)=> setTgPass(e.target.value)}
+  />
+</div>
+<button className="normal">Ввести код</button>
+<DarkMode/>
+</form>
+:
+<form onSubmit={(e) => signIn(e)} className="login__form">
       <div className="form__control">
         <label>Логін</label>
         <input
@@ -76,6 +122,9 @@ const LoginForm = () => {
       <button className="normal">Увійти</button>
       <DarkMode/>
     </form>
+
+}
+</>
   );
 };
 
