52a1c223e1e37292ace265ce572f53159f60d725
tatarynrm <tatarynrm@gmail.com>
2023-07-28 11:51:52 +0300

    ok

--- src/components/zap/AddZap.jsx
+++ src/components/zap/AddZap.jsx
@@ -7,6 +7,7 @@ import { fetchZap, addReduxZap } from "../../redux/slices/zap";
 import socket from "../../utils/socket";
 import { beep } from "../../helpers/audio";
 import { editZapAddSlice } from "../../redux/slices/edit";
+import request from 'axios'
 // import { io } from "socket.io-client";
 const AddZap = ({ selectedGroup, showAddZap, setAddZap }) => {
   const zap = useSelector((state) => state.zap.zap.items);
@@ -16,6 +17,29 @@ const AddZap = ({ selectedGroup, showAddZap, setAddZap }) => {
   const [zapText, setZapText] = useState("");
   const [zapType, setZapType] = useState(null);
   const dispatch = useDispatch();
+
+
+
+useEffect(()=>{
+  let base_url = "https://maps.googleapis.com/maps/api/place/details/json/language=uk"
+  const  place_id = "ChIJBUVa4U7P1EAR_kYBF9IxSXY"
+  const api_key = "AIzaSyCL4bmZk4wwWYECFCW2wqt7X-yjU9iPG2o"
+let params = {
+    "place_id": place_id,
+    // "fields": "name,formatted_address,rating",
+    "key": api_key
+}
+
+const getByPlaceId  = async ()=>{
+  try {
+   const response = await request.post(base_url,params)
+   console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+  getByPlaceId()
+},[])
   const handleSubmitAddZap = async (e) => {
     e.preventDefault();
     const object = {
@@ -43,14 +67,14 @@ const AddZap = ({ selectedGroup, showAddZap, setAddZap }) => {
       console.log(error);
     }
   };
-
+console.log(zav);
   useEffect(() => {}, [zap]);
   return (
     <form onSubmit={handleSubmitAddZap} className="add__zap">
       <div className="form__control">
         <GooglePlacesAutocomplete
           className="okkk"
-          apiKey="AIzaSyDddHSvr8KFFahBGyqLCQVxpjCsFw-p5ek"
+          apiKey="AIzaSyCL4bmZk4wwWYECFCW2wqt7X-yjU9iPG2o"
           apiOptions={{
             language: "uk",
           }}
@@ -64,7 +88,7 @@ const AddZap = ({ selectedGroup, showAddZap, setAddZap }) => {
       </div>
       <div className="form__control">
         <GooglePlacesAutocomplete
-          apiKey="AIzaSyDddHSvr8KFFahBGyqLCQVxpjCsFw-p5ek"
+          apiKey="AIzaSyCL4bmZk4wwWYECFCW2wqt7X-yjU9iPG2o"
           apiOptions={{
             language: "uk",
           }}
@@ -74,7 +98,9 @@ const AddZap = ({ selectedGroup, showAddZap, setAddZap }) => {
             placeholder: "Місто вивантаження",
           }}
         />
+   
       </div>
+   
       <div className="form__control">
         <textarea
           onChange={(e) => setZapText(e.target.value)}
